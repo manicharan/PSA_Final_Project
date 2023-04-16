e306326daa288c5e1a3ee4491f7b223f57c49bf5
Prashanth_Vobilishetty <113191587+VobilishettyPrashanth@users.noreply.github.com>
2023-04-15 21:27:01 -0400

    Updating local branch (#19)
    * Added logic to find odd degree vertices in MST
    
    * Updating local branch
    
    * added logic to findMinimumWeightPerfectMatching from given odd degree vertices
    
    * Added logic to combine MST and Matching Graph to form Eulerian Graph
    
    * Added logic to combine MST and Matching graph to form Eulerian Graph
    
    * Refactored the code and added minor optimizations
    
    ---------
    
    Co-authored-by: Nishaanth-44 <114043295+Nishaanth-44@users.noreply.github.com>
    Co-authored-by: manicharan <114465171+manicharan@users.noreply.github.com>
    Co-authored-by: Mani Charan <loka.m@northeastern.edu>

--- src/main/java/travellingsalesman/MST.java
+++ src/main/java/travellingsalesman/MST.java
@@ -1,10 +1,6 @@
 package travellingsalesman;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class MST {
@@ -49,4 +45,55 @@ public class MST {
 	        return mst;
 	    }
 
+	public static HashSet<Integer> findOddDegreeVertices(Graph mst,Graph graph) {
+		HashSet<Integer> oddVertices = new HashSet<>();
+		for (int i = 0; i < mst.getNumVertices(); i++) {
+			Vertex v=mst.getVertex(i);
+			int deg= mst.getDegree(v.getId());
+			if (mst.getDegree(mst.getVertex(i).getId()) % 2 == 1) {
+//                System.out.println("Degree " +i+" "+mst.getDegree(mst.getVertex(i).getId()));
+				oddVertices.add(graph.getIndex(mst.getVertex(i).getId()));
+			}
+		}
+		return oddVertices;
+	}
+
+	public static Graph findMinimumWeightPerfectMatching(HashSet<Integer> oddVertices,Graph graph) {
+		Graph matching = new Graph();
+
+        // If there are no odd degree vertices, return an empty matching
+        if (oddVertices.isEmpty()) {
+            return matching;
+        }
+
+        // Create a subgraph containing only the odd degree vertices and their edges
+        Graph oddGraph = new Graph();
+        for(int v:oddVertices){
+            oddGraph.addVertex(graph.getVertex(v));
+            matching.addVertex(graph.getVertex(v));
+        }
+        for(Edge e: graph.getEdges()){
+            if(oddVertices.contains(graph.getIndex(e.getU().getId())) &&
+                    oddVertices.contains(graph.getIndex(e.getV().getId())))
+                oddGraph.addEdge(e);
+        }
+//        System.out.println(oddGraph.getNumEdges() +" "+oddGraph.getNumVertices());
+        for(Edge e:oddGraph.getEdges()){
+//            System.out.println(e);
+        }
+        List<Edge> oddEdges = oddGraph.getEdges();
+        Collections.sort(oddEdges);
+        HashSet<Vertex> oddVer = new HashSet<Vertex>();
+        for(Vertex v: oddGraph.getVertices()){
+            oddVer.add(v);
+        }
+        for(Edge e:oddEdges){
+            if(oddVer.contains(e.getU()) && oddVer.contains(e.getV())){
+                matching.addEdge(e);
+                oddVer.remove(e.getU());
+                oddVer.remove(e.getV());
+            }
+        }
+        return matching;
+	}
 }
