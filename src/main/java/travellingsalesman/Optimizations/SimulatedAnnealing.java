07977695f7a07a6d8d5930aa90791425fd25b2e2
Nishaanth-44 <114043295+Nishaanth-44@users.noreply.github.com>
2023-04-18 00:58:18 -0400

    Modified SimulatedAnnealing Class

--- src/main/java/travellingsalesman/Optimizations/SimulatedAnnealing.java
+++ src/main/java/travellingsalesman/Optimizations/SimulatedAnnealing.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Random;
 
 import travellingsalesman.TSP.TSP;
+import travellingsalesman.graph.Graph;
 
 public class SimulatedAnnealing {
 
@@ -13,11 +14,9 @@ public class SimulatedAnnealing {
     private static final double COOLING_RATE = 0.999;
     private static final int MAX_ITERATIONS = 10000;
 
-    public static List<Integer> optimize(List<Integer> path) {
-        List<Integer> currentPath = new ArrayList<>(path);
-        double currentDistance = TSP.calculateDistance(currentPath);
-        List<Integer> bestPath = new ArrayList<>(currentPath);
-        double bestDistance = currentDistance;
+    public static double optimize(List<Integer> bestPath, double bestDistance, Graph graph) {
+        List<Integer> currentPath = new ArrayList<>(bestPath);
+        double currentDistance = TSP.calculateDistance(currentPath, graph);
         double temperature = INITIAL_TEMPERATURE;
         Random random = new Random();
         // Apply the 2-opt method to the path until no further improvements can be made
@@ -25,19 +24,19 @@ public class SimulatedAnnealing {
         int count=0;
         while (improved) {
             improved = false;
-            for (int i = 1; i < path.size() - 2; i++) {
-                for (int j = i + 2; j < path.size()-1 ; j++) {
-                    List<Integer> newPath = TSP.twoOptSwap(path, i, j);
-                    double newDistance = TSP.calculateDistance(newPath);
+            for (int i = 1; i < bestPath.size() - 2; i++) {
+                for (int j = i + 2; j < bestPath.size()-1 ; j++) {
+                    List<Integer> newPath = TacticalOptimizations.twoOptSwap(bestPath, i, j);
+                    double newDistance = TSP.calculateDistance(newPath, graph);
                     double acceptanceProbability = acceptanceProbability(currentDistance, newDistance, temperature);
                     double ran = random.nextDouble();
                     boolean isValid = acceptanceProbability > ran;
-                    if (newDistance < currentDistance || isValid) {  
+                    if (newDistance < currentDistance || isValid) {
                     	improved = true;
                         currentPath = newPath;
                         currentDistance = newDistance;
                         if (currentDistance < bestDistance) {
-                        	
+
                             bestPath = new ArrayList<>(currentPath);
                             bestDistance = currentDistance;
                         }
@@ -49,20 +48,8 @@ public class SimulatedAnnealing {
             	break;
             System.out.println(count++);
         }
-        return bestPath;
+        return bestDistance;
     }
-
-
-//    private static List<Integer> twoOptSwap(List<Integer> path, int i, int j) {
-//        if (i == 0 || j == path.size() - 1)
-//            return path;
-//        List<Integer> newPath = new ArrayList<>(path.subList(0, i));
-//        List<Integer> subList = path.subList(i, j + 1);
-//        Collections.reverse(subList);
-//        newPath.addAll(subList);
-//        newPath.addAll(path.subList(j + 1, path.size()));
-//        return newPath;
-//    }
 
     private static double acceptanceProbability(double currentDistance, double newDistance, double temperature) {
         if (newDistance < currentDistance) {
